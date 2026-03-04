7dc9efe658274e2118b1f3d8bd8dc2beefd6db8e
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-03-04 21:12:33 +0300

    Добавление сайдбара с категориями и страницы категорий. Работа с переменными в URL запросах и использование имен маршрутов

--- MySite/news/urls.py
+++ MySite/news/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views  # Импортируем views из текущего каталога
+from .views import get_category
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.test, name='test'),
-    path('test2/', views.test2, name='test2'),
+    path('', views.index, name='home'),
+    path('/category/<int:category_id>/', get_category,  name='category'),
+#   path('test/', views.test, name='test'),
+#   path('test2/', views.test2, name='test2'),
+
 
 ]
