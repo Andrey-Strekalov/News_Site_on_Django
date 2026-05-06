d4cdec9bfeadaba54e7ec9307c889f0db69d6976
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2026-05-06 23:06:21 +0300

    Lab_11: добавить форму добавления новостей с валидацией и Bootstrap

--- MySite/news/urls.py
+++ MySite/news/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views  # Импортируем views из текущего каталога
-from .views import get_category
-
+from .views import get_category, add_news
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('/category/<int:category_id>/', get_category,  name='category'),
     path('news/<int:news_id>/', views.view_news, name='view_news'),
-    #   path('test/', views.test, name='test'),
-#   path('test2/', views.test2, name='test2'),
-
+    path('news/add-news', add_news, name='add_news'),
 
 ]
